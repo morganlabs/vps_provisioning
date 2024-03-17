c1fc2188188a164cf3736c5e61a674279c012e9e
Morgan Jones <me@morganlabs.dev>
2024-03-17 15:52:08 +0000

    feat: stuff

--- bin/get_server_ip.sh
+++ bin/get_server_ip.sh
@@ -5,4 +5,9 @@ cd "$(dirname "$0")/.."
 VAULT_ITEMS=`ansible-vault decrypt vault --output=-`
 SERVER_IP=`jq -r '.server_ip' <<< "$VAULT_ITEMS"`
 
-echo "$SERVER_IP"
+if [[ "$1" = "with_port" ]]; then
+    SERVER_PORT=`jq -r '.ssh_port' <<< "$VAULT_ITEMS"`
+    SERVER_PORT=":$SERVER_PORT"
+fi
+
+echo "$SERVER_IP$SERVER_PORT"
